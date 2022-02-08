4b7ffaffd49196af8d94cf02c373f538ba63d16a
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-02-08 16:27:27 +0100

    toUpperCase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 let color = document.getElementById("color");
 function changeColor() {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16).toUpperCase();
     document.body.style.backgroundColor = "#" + randomColor;
     color.innerHTML = "#" + randomColor;
 }
