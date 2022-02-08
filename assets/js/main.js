e2ea6eb6551cbf9043bfb0087d43e7ae2ae5efa8
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-02-08 15:53:06 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+let color = document.getElementById("color");
+function changeColor() {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+}
