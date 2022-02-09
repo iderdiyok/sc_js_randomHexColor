63161fab9c3b955b18bfd6c0a6706caa7cf956e9
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-02-09 11:40:40 +0100

    color story - reset button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 
 let color = document.getElementById("color");
+let out = document.getElementById("output")
+
 function changeColor() {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16).toUpperCase();
     document.body.style.backgroundColor = "#" + randomColor;
     color.innerHTML = "#" + randomColor;
+    out.classList.add("shadow", "bg-light")
+    out.innerHTML += `<div class="col"><h4 style="color: ${color.innerHTML}">${color.innerHTML}</h4></div>`
 }
+
+
+
+
